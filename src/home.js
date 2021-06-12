e84ad913438aada47a470d901b50d6224e5da05d
khalil-sam <67326446+khalil-sam@users.noreply.github.com>
2021-06-11 23:28:43 -0500

    blue bar 1

--- src/home.js
+++ src/home.js
@@ -7,21 +7,21 @@ class home extends React.Component {
 
         return (
             <div>
-    <section class = "hero" className = "hero">
+    <section className = "hero" class = "hero">
         <div class="video-wrapper">
-            <div class="wrapper">
-                <iframe title="Video_Website Video 2021_Web_16x9_Draft 1" src="https://player.vimeo.com/video/516845835?dnt=1&amp;app_id=122963&amp;controls=0&amp;hd=1&amp;autohide=1&amp;background=1&amp;autoplay=1" width="100%" height="900" border-left-width= "900"  allow="autoplay; fullscreen; picture-in-picture" allowFullScreen={false}>
+            <div className="wrapper">
+                <iframe title="Video_Website Video 2021_Web_16x9_Draft 1" src="https://player.vimeo.com/video/516845835?dnt=1&amp;app_id=122963&amp;controls=0&amp;hd=1&amp;autohide=1&amp;background=1&amp;autoplay=1" width="100%" height="900" border-left-width= "900"  allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="true">
                 </iframe>
             </div>
             
         </div>
-        <div className="overlay"></div>
+        <div class="overlay"></div>
         <h1 id="text"> United Nations Resolutions </h1>
         <p id ="smallt"> Browse UN General Assembly resolutions online. View resolutions by country and year! </p>
         <img id = "imglogo" src = "https://www.pngkey.com/png/full/139-1391666_un-logo-white-united-nations-logo-white.png" />
     </section>
         <div className="grid-container">
-            <div className="grid-item"> 
+            <div class="grid-item"> 
                 <Chart
                 width={'100%'}
                 height={'100%'}
@@ -42,28 +42,28 @@ class home extends React.Component {
                 rootProps={{ 'data-testid': '1' }}
               />
             </div>
-            <div className="grid-item-blue"> 
+            <div class="grid-item-blue"> 
             <p id="text-white"> This website features 6203 resolutions From 1946 to 2019.</p>
             <p id="text-smaller"> There are six main resolution categories: the Palestinian conflict, nuclear weapons, arms control, colonialism, human rights, and economic development.</p>
             <p id="text-smaller"> The resolutions page in this website allows you to choose a specific resolution from a chosen year. To access the page please click on the button below</p>
              <Link to="/resolutions">
-     <button  className="myButton"type="button">
+     <button  class="myButton"type="button">
           See Resolutions
      </button>
  </Link>
             </div>
-            <div className="grid-item-blue"> 
+            <div class="grid-item-blue"> 
             <p id="text-white"> This website features 202 countries.</p>
             <p id="text-smaller">  The website includes 202 countries including former sovereign states, such as the German Democratic Republic, Czechoslovakia, Zanzibar, and Serbia and Montenegro. </p>
             <p id="text-smaller">To access the countries page click on the button below. Once you choose a country you have to select a year from the dropdown menu. </p>
             <Link to="/countries">
-     <button  className="myButton"type="button">
+     <button  class="myButton"type="button">
           See Countries
      </button>
             </Link>
             </div>
-            <div className="grid-item"> 
-            <div className = "white"> 
+            <div class="grid-item"> 
+            <div class = "white"> 
             <p id="big-text"> More Details </p>
             <p id="detail-text">This website is made by Khalil Samoud and Chase Duvall as a project for <a href="https://cs396-web-dev.github.io/spring2021/"> CS-396 Web Development </a> .</p>
             <p id="detail-text"> This Website is not officially affiliated with the United Nations.For more details on our data, please visit <a href="https://dataverse.harvard.edu/dataset.xhtml?persistentId=hdl:1902.1/12379"> Harvard's dataverse.</a> </p>
@@ -74,3 +74,4 @@ class home extends React.Component {
             </div>)
 }}
 export default home;
+
